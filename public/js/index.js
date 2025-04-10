adb6041c0abc80374e728961ca0e3ec864bbce2c
Israel Nunes <israelnetonunes@gmail.com>
2025-04-10 16:45:35 -0300

    First commit - only navbar and header

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".progress-slide-carousel", {
+    loop: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    slidesPerView: 1,
+    spaceBetween: 20,
+});
